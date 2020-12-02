7a0550a7fe035ca16780800fe8b4c1a5edc35ad2
Torbjörn Einarsson <torbjorn@einarssons.se>
2020-12-02 21:29:49 +0100

    Dec 2 finished

--- dec2/m.py
+++ dec2/m.py
@@ -1,9 +1,45 @@
+import re
+
+pattern = re.compile(r"(?P<min>\d+)-(?P<max>\d+) (?P<char>\w): (?P<pwd>\w+)")
 
 def check_password(min_len:int, max_len:int, char:str, pwd:str) -> bool:
     "Check if password is valid according to rule defined by min, max, char."
-    return False
+    n=0
+    for c in pwd:
+        if c == char:
+            n += 1
+    return min_len <= n <= max_len
 
+def xor(a:bool, b:bool)->bool:
+    return (a or b) and not (a and b)
 
-def parse_line(line:str) -> {int, int, str, str}:
+def check_password2(idx1:int, idx2:int, char:str, pwd:str) -> bool:
+    "Check if password is valid according to rule defined by idx1, idx2, char."
+    return xor(pwd[idx1-1] == char, pwd[idx2-1] == char)
+
+def parse_line(line:str) -> {str:int, str:int, str:str, str:str}:
     "Parse line of form min-max char: pwd and return min, max, char, pwd."
+    mobj = pattern.match(line)
+    if mobj:
+        gd = mobj.groupdict()
+        gd['min'] = int(gd['min'])
+        gd['max'] = int(gd['max'])
+        return gd
     return {"min":0, "max": 0, "char": "", "pwd": ""}
+
+def main(file_name):
+    with open(file_name) as ifh:
+        n1 = 0
+        n2 = 0
+        for line in ifh:
+            ld = parse_line(line)
+            if check_password(ld['min'], ld['max'], ld['char'], ld['pwd']):
+                n1 += 1
+            if check_password2(ld['min'], ld['max'], ld['char'], ld['pwd']):
+                n2 += 1
+        print(f"n1={n1}")
+        print(f"n2={n2}")
+
+if __name__ == '__main__':
+    main('data.txt')
+
--- dec2/test.py
+++ dec2/test.py
@@ -19,6 +19,24 @@ class TestDec2(unittest.TestCase):
             result = m.check_password(d["min"], d["max"], d["char"], d["pwd"])
             self.assertEqual(result, c.want, c.name)
 
+    def test_parse_line(self):
+        cases = [
+            TestCase("first_line", "1-3 a: abcde", {"min": 1, "max": 3, "char": "a", "pwd": "abcde"}),
+        ]
+        for c in cases:
+            result = m.parse_line(c.input)
+            self.assertEqual(result, c.want, c.name)
+
+    def test_check_password2(self):
+        cases = [
+            TestCase("case 1", {"min": 1, "max": 3, "char": "a", "pwd": "abcde"}, True),
+            TestCase("case 2", {"min": 1, "max": 3, "char": "b", "pwd": "cdefg"}, False),
+            TestCase("case 3", {"min": 2, "max": 9, "char": "c", "pwd": "ccccccccc"}, False)
+        ]
+        for c in cases:
+            d = c.input
+            result = m.check_password2(d["min"], d["max"], d["char"], d["pwd"])
+            self.assertEqual(result, c.want, c.name)
 
 if __name__ == '__main__':
     unittest.main()
