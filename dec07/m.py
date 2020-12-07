c05e130955698494326c532207a0b01ebc731b59
Torbjörn Einarsson <torbjorn@einarssons.se>
2020-12-07 21:53:04 +0100

    Dec 07 finished

---
+++ dec07/m.py
@@ -0,0 +1,60 @@
+
+import re
+
+from collections import namedtuple
+
+Rule = namedtuple('Rule', ['bag', 'subs'])
+Sub = namedtuple('Sub', ['nr', 'bag'])
+
+
+def parse_rule(rule_line:str) -> Rule:
+    mobj = re.match(r"(\w+ \w+) bags contain", rule_line)
+    if mobj is None:
+        raise ValueError("No initial color")
+    bag = mobj.group(1)
+    rest_of_line = rule_line[len(mobj.group(0)):]
+    subs = []
+    for mobj in re.finditer(r" (\d+) (\w+ \w+) bags?[,.]", rest_of_line):
+        subs.append(Sub(int(mobj.group(1)), mobj.group(2)))
+    return Rule(bag, subs)
+
+def find_containers(bag:str, rules:[])->[]:
+    outers = set()
+    cores = [bag]
+    while True:
+        try:
+            core = cores.pop()
+        except IndexError:
+            break
+        for rule in rules:
+            if rule.bag in outers:
+                continue
+            for sub in rule.subs:
+                if sub.bag == core:
+                    cores.append(rule.bag)
+        if core != bag:
+            outers.add(core)
+    return outers
+
+def count_subs(bag:str, rules:dict)->int:
+    count = 1
+    subs = rules[bag]
+    for sub in subs:
+        count += count_subs(sub.bag, rules) * sub.nr
+    return count
+
+def main():
+    with open('rules.txt') as ifh:
+        lines = ifh.read().splitlines()
+        rules = [parse_rule(raw_rule) for raw_rule in lines]
+        containers = find_containers('shiny gold', rules)
+        rulesdict = dict(rules)
+        count = count_subs('shiny gold', rulesdict)
+        count -= 1  # Don't count top bag
+        print(len(containers))
+        print(count)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ dec07/test.py
@@ -0,0 +1,65 @@
+import unittest
+from collections import namedtuple
+
+import m
+
+test_rules = '''\
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.\
+'''.splitlines()
+
+parsed_rules = [
+    m.Rule('light red', [(1, 'bright white'), (2, 'muted yellow')]),
+    m.Rule('dark orange', [(3, 'bright white'), (4, 'muted yellow')]),
+    m.Rule('bright white', [(1, 'shiny gold')]),
+    m.Rule('muted yellow', [(2, 'shiny gold'), (9,  'faded blue')]),
+    m.Rule('shiny gold', [(1, 'dark olive'), (2, 'vibrant plum')]),
+    m.Rule('dark olive', [(3, 'faded blue'), (4, 'dotted black')]),
+    m.Rule('vibrant plum' ,[(5, 'faded blue'), (6, 'dotted black')]),
+    m.Rule('faded blue', []),
+    m.Rule('dotted black', [])
+]
+
+test_rules2 = '''\
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.\
+'''.splitlines()
+
+
+TestCase = namedtuple("TestCase", ["text", "output"])
+
+class TestDec7(unittest.TestCase):
+
+    def test_parse_rules(self):
+        for i, raw_rule in enumerate(test_rules):
+            parsed_rule = m.parse_rule(raw_rule)
+            self.assertEqual(parsed_rule, parsed_rules[i], raw_rule)
+
+    def test_find_containers(self):
+        rules = [m.parse_rule(raw_rule) for raw_rule in test_rules]
+        containers = m.find_containers("shiny gold", rules)
+        self.assertEqual(len(containers), 4, containers)
+
+    def test_count_subs(self):
+        rules = [m.parse_rule(raw_rule) for raw_rule in test_rules2]
+        rulesdict = dict(rules)
+        count = m.count_subs('shiny gold', rulesdict)
+        count -= 1  # Don't count top bag
+        self.assertEqual(count, 126)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
