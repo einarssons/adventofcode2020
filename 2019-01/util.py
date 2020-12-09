386854a555e21368aa7d7e24e369b03d48c92a2d
Torbjörn Einarsson <torbjorn@einarssons.se>
2020-12-09 07:31:10 +0100

    remove 2019 puzzle

--- 2019-01/util.py
+++
@@ -1,8 +0,0 @@
-def readNumbers(fileName):
-    numbers = []
-    with open(fileName) as ifh:
-        for line in ifh:
-            nr = int(line)
-            numbers.append(nr)
-    return numbers
-
